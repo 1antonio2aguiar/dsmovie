bacb8d64ee3f0da824744744ac9908064d0a3198
Antonio Aguiar <antonio.oliveira@codiub.com.br>
2022-05-12 10:44:42 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
-
-import NavBar from './components/navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from './pages/listing';
+import Form from 'pages/form';
+import Navbar from "components/navbar";
 
 function App() {
-  return(
-    <NavBar />
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
