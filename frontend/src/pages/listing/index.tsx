e72b39f32152d3df6725605ec5809aa70baaa9ad
Antonio Aguiar <antonio.oliveira@codiub.com.br>
2022-05-12 13:57:04 -0300

    Movie Card

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,8 +1,54 @@
 import Pagination from "components/pagination";
+import MovieCard from "components/moviecard";
 
 function Listing(){
 
-    return <Pagination />
+    return (
+        <>
+            <Pagination />
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6 col-lg-4 col-xl-3">
+                            <MovieCard />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                    </div>
+                </div>
+
+        </>
+    )
 }
 
 export default Listing;
